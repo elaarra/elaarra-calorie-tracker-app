4eb90d04966d561c81603e748c3f2bfdf501f8ef
Caroline Milton <caroline.milton@elaarra.com>
2026-05-11 18:37:59 +0100

    add remaining project files

--- lib/screens/main_shell.dart
+++ lib/screens/main_shell.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../utils/theme.dart';
 import 'home/home_screen.dart';
